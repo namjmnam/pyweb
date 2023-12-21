7216d291f9992dc6051cad04affc19c85b079aad
namjmnam <namjmnam@naver.com>
2023-12-21 14:23:25 +0900

    Download feature added

--- server.py
+++ server.py
@@ -3,17 +3,35 @@ import socketserver
 import os
 
 PORT = 8000
+DIRECTORY = "./sharedfiles"
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        # Print the contents of the current directory
-        # print("Current directory contents:")
-        # for file in os.listdir('.'):
-        #     print(file)
-
         if self.path == '/':
+            # Serve the main investment page
             self.path = 'oil_investment.html'
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+            return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+        elif self.path == '/files':
+            # Serve the file listing page
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            html = '<html><head><title>File Downloads</title></head><body>'
+            html += '<h1>Available Files</h1><ul>'
+            for filename in os.listdir(DIRECTORY):
+                html += f'<li><a href="{DIRECTORY}/{filename}" download>{filename}</a></li>'
+            html += '</ul></body></html>'
+
+            self.wfile.write(html.encode('utf-8'))
+
+        else:
+            # Handle other requests normally
+            return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+# Set the directory to serve
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # Set up the HTTP server
 with socketserver.TCPServer(("", PORT), MyHttpRequestHandler) as httpd:
