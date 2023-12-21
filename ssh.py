57a619984564ad3cc3f2c23ed5e79510c768f263
namjmnam <namjmnam@naver.com>
2023-12-21 16:34:08 +0900

    Added SSH

--- ssh.py
+++ ssh.py
@@ -1,56 +1,30 @@
 import paramiko
-import socket
-import threading
-import sys
 
-# You need to generate these keys (e.g., using ssh-keygen)
-host_key = paramiko.RSAKey(filename='test_rsa.key')
+def generate_ssh_key_pair(key_type=paramiko.RSAKey, bits=2048):
+    """
+    Generate an SSH key pair.
+    
+    Args:
+    - key_type: The type of key to generate (paramiko.RSAKey or paramiko.DSSKey).
+    - bits: Number of bits in the key (default: 2048).
 
-class Server (paramiko.ServerInterface):
-    def __init__(self):
-        self.event = threading.Event()
+    Returns:
+    - A tuple (private_key_str, public_key_str) containing the private and public keys as strings.
+    """
+    key = key_type.generate(bits=bits)
+    private_key_str = key.write_private_key_str()
+    public_key_str = f"{key.get_name()} {key.get_base64()}"
+    return private_key_str, public_key_str
 
-    def check_channel_request(self, kind, chanid):
-        if kind == 'session':
-            return paramiko.OPEN_SUCCEEDED
-        return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+private_key, public_key = generate_ssh_key_pair()
 
-    def check_auth_password(self, username, password):
-        if (username == 'user') and (password == 'password'):
-            return paramiko.AUTH_SUCCESSFUL
-        return paramiko.AUTH_FAILED
+# Save the private key to 'test_rsa.key'
+with open("test_rsa.key", "w") as private_file:
+    private_file.write(private_key)
 
-# Create the SSH server
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind(('localhost', 22))
-server.listen(100)
+# Optionally, save the public key to a file (e.g., 'test_rsa.pub')
+with open("test_rsa.pub", "w") as public_file:
+    public_file.write(public_key)
 
-print("Listening for connection...")
-client, addr = server.accept()
-
-try:
-    transport = paramiko.Transport(client)
-    transport.add_server_key(host_key)
-    server = Server()
-    try:
-        transport.start_server(server=server)
-    except paramiko.SSHException:
-        print("SSH negotiation failed.")
-
-    channel = transport.accept(20)
-    if channel is None:
-        print("No channel.")
-        sys.exit(1)
-
-    print("Authenticated!")
-
-    channel.close()
-
-except Exception as e:
-    print(f"Caught exception: {str(e)}")
-    try:
-        transport.close()
-    except:
-        pass
-    sys.exit(1)
+print("Private key saved to 'test_rsa.key'")
+print("Public key saved to 'test_rsa.pub'")
