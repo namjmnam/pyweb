3b6d3dac97ff5dd8a69830037ac58d5f5a98ef47
namjmnam <namjmnam@naver.com>
2023-12-22 13:48:35 +0900

    Added a feature to retrieve variables from FTP

--- varhandler.py
+++ varhandler.py
@@ -23,8 +23,30 @@ def send_var_to_ftp(variable, ftp_server, remote_path, ftp_username='anonymous',
         # Use STOR command to upload the file
         ftp.storbinary('STOR ' + remote_path, in_memory_file)
 
+def retrieve_vars_from_ftp(ftp_server, remote_path, ftp_username='anonymous', ftp_password='user@example.com'):
+    """
+    Retrieve a file from an FTP server, load the variables, and print them.
+
+    :param ftp_server: FTP server address.
+    :param remote_path: Remote path of the file to be retrieved.
+    :param ftp_username: FTP username, defaults to 'anonymous'.
+    :param ftp_password: FTP password, defaults to 'user@example.com'.
+    """
+    # Connect to the FTP server
+    with ftplib.FTP(ftp_server) as ftp:
+        ftp.login(user=ftp_username, passwd=ftp_password)
+        
+        # Retrieve the file into an in-memory file
+        in_memory_file = io.BytesIO()
+        ftp.retrbinary('RETR ' + remote_path, in_memory_file.write)
+        in_memory_file.seek(0)  # Rewind the file to the beginning
+
+        # Load the variables from the in-memory file
+        variables = pickle.load(in_memory_file)
+        print(variables)
+
 # Example usage
-my_data = {'key1': 'value1', 'key2': 'value2'}
+my_data = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
 
 # FTP server details
 ftp_server_address = 'localhost'
@@ -32,3 +54,4 @@ remote_file_path = 'remote_filename.pkl'
 
 # Save the variable and send it to FTP using anonymous login
 send_var_to_ftp(my_data, ftp_server_address, remote_file_path)
+retrieve_vars_from_ftp(ftp_server_address, remote_file_path)
